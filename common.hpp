855287df7dad2a67da6393d1fb1ec17bd4e333dc
Yuki Sakamoto <sakamotoyuki.jpn+gh@gmail.com>
2017-07-06 20:11:49 +0900

    add constants related on the scale(length) into the common.hpp

--- common.hpp
+++ common.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "Eigen/Core"
 
 // numerical precision: Double 
@@ -7,3 +8,30 @@ typedef Eigen::Vector3d Vector3Real;
 typedef Eigen::Vector2d Vector2Real;
 typedef Eigen::MatrixXd MatrixXReal;
 
+// Constant Values
+static const REAL factor_Bohr2Angstrom = 0.529177249;
+static const REAL factor_Angstrom2Bohr = 1./factor_Bohr2Angstrom;
+
+inline REAL
+Bohr2Angstrom(REAL x)
+{
+    return x*factor_Bohr2Angstrom;
+}
+
+inline Vector3Real
+Bohr2Angstrom(Vector3Real r)
+{
+    return r * factor_Bohr2Angstrom;
+}
+
+inline REAL
+Angstrom2Bohr(REAL x)
+{
+    return x/factor_Bohr2Angstrom;
+}
+
+inline Vector3Real
+Angstrom2Bohr(Vector3Real r)
+{
+    return r / factor_Bohr2Angstrom;
+}
